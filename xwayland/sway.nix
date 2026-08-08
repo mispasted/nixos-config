c2a8f947a1c9fb57384cbf35c6a06f9ef1588af7
mispasted <mispasted@protonmail.com>
2026-08-08 11:21:13 -0600

    Finished adjusting sway

--- xwayland/sway.nix
+++ xwayland/sway.nix
@@ -5,31 +5,35 @@
       wl-clipboard # Copy/Paste functionality.
       mako # Notification utility.
       swayfx
+      kanshi
   ];
 
   # Enables Gnome Keyring to store secrets for applications. 
   services.gnome.gnome-keyring.enable = true;
 
   # Enable Sway
-/*   programs.sway = 
+   programs.sway = 
   {
     enable = true;
-    # package = pkgs.sway;
+    package = pkgs.swayfx;
     wrapperFeatures.gtk = true;
-    # xwayland.enable = true;
 
     # Nice list: https://github.com/swaywm/sway/wiki/Useful-add-ons-for-sway
     # Currently the default
-    # extraPackages = with pkgs;
-    #   [
-    #     brightnessctl 
-    #     foot
-    #     grim
-    #     pulseaudio
-    #     swayidle
-    #     swaylock
-    #     wmenu
-    #   ];
   };
-  # programs.waybar.enable = true */
+
+  # kanshi systemd service
+  # Enables monitor hot swapping
+systemd.user.services.kanshi = {
+  description = "kanshi daemon";
+  environment = {
+    WAYLAND_DISPLAY="wayland-1";
+    DISPLAY = ":0";
+  }; 
+  serviceConfig = {
+    Type = "simple";
+    ExecStart = ''${pkgs.kanshi}/bin/kanshi -c kanshi_config_file'';
+  };
+};
+
 }
