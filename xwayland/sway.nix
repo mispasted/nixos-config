8bc755ccedeccc7b2c0af3967f8a0b4367d1c02e
mispasted <mispasted@protonmail.com>
2026-08-08 00:32:21 -0600

    removed faulty sway code

--- xwayland/sway.nix
+++ xwayland/sway.nix
@@ -32,22 +32,4 @@
     #   ];
   };
   # programs.waybar.enable = true */
-
-  wayland.windowManager.sway = {
-  enable = true;
-  package = pkgs.swayfx;
-  
-  # Needed to build without errors.
-  checkConfig = false;
-    
-  # SwayFX options must be configured through extraConfig.
-  extraConfig = ''
-    shadows enable
-    corner_radius 11
-    blur_radius 7
-    blur_passes 2
-  '';
-};
-
-
 }
